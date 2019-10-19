265ac548a46a3ff8aa50e810917fdd6b24f83719
Jalin Wiley <jmt.wiley@gmail.com>
2019-10-18 21:31:34 -0400

    I added a lot should've pushed

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+  {
+    name: "Jason Stathom",
+    photo: "assets/images/Jason Stathom.jpg",
+    scores: [4, 5, 5, 3, 4, 1, 2, 1, 3, 5]
+  },
+  {
+    name: "Prince",
+    photo: "assets/images/Prince.jpg",
+    scores: [2, 1, 1, 3, 5, 2, 3, 1, 3, 4]
+  },
+  {
+    name: "Mathew McConaughey",
+    photo: "assets/images/Matthew-McConaughey.jpg",
+    scores: [4, 5, 4, 5, 2, 4, 1, 3, 1, 2]
+  },
+  {
+    name: "Jet Li",
+    photo: "assets/images/Jet-Li.jpg",
+    scores: [1, 3, 3, 1, 2, 4, 5, 5, 1, 3]
+  },
+  {
+    name: "Sza",
+    photo: "assets/images/sza01.jpg",
+    scores: [5, 1, 1, 4, 5, 2, 3, 2, 4, 1]
+  },
+  {
+    name: "Julianne Moore",
+    photo: "assets/images/JulianneMoore.jpg",
+    scores: [3, 4, 4, 2, 3, 5, 1, 5, 2, 4]
+  }
+];
+
+module.exports = friends;
